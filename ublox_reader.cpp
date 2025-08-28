a2e3bc2bbce1f01488271cc3ba69b354015884c7
IneshAg <ineshag123@gmail.com>
2025-08-28 23:12:15 +0530

    Final working version: GPS decoding, path planning, odometry

--- ublox_reader.cpp
+++ ublox_reader.cpp
@@ -4,19 +4,20 @@
 
 typedef struct
 {
-    uint32_t iTOW;
-    uint16_t year;
-    uint8_t month, day, hour, min, sec;
-    uint8_t fixType;
-    int32_t lon;
-    int32_t lat;
-} UBXNavPVT;
+    uint32_t iTOW; // It tells us the variable is unsigned int and has 32 bits and it stores the Gps Time in milliseconds
+    uint16_t year; //  It tells us the Year
+    uint8_t month, day, hour, min, sec; //  It tells us the month,day,hr etc
+    uint8_t fixType;  //  It will have a single byte dedicated to tell us in a way whether it is true or false but just instead of 0 and 1 it has 0,1,2 
+    int32_t lon;     // It tells us the longitude
+    int32_t lat;     //  It tells us the latitude
+} UBXNavPVT; // Name of the struct
 
-UBXNavPVT decodeUBXNavPVT(const uint8_t *buf)
+UBXNavPVT decodeUBXNavPVT(const uint8_t *buf) // decodeUBXNavPVT here would take the sequence of bytes it receives and convert into the above variables
 {
     UBXNavPVT data;
-    std::memcpy(&data.iTOW, buf + 0, sizeof(data.iTOW));
-    std::memcpy(&data.year, buf + 4, sizeof(data.year));
+    std::memcpy(&data.iTOW, buf + 0, sizeof(data.iTOW)); //It copies the first 4 bytes data from the buffer into the iTOW
+    std::memcpy(&data.year, buf + 4, sizeof(data.year)); // It does the same as above just the starting position is index 4 of buffer
+    // Rest does the same in order 
     data.month = buf[6];
     data.day = buf[7];
     data.hour = buf[8];
@@ -28,5 +29,5 @@ UBXNavPVT decodeUBXNavPVT(const uint8_t *buf)
     return data;
 }
 
-double getLatitude(UBXNavPVT &d) { return d.lat / 1e7; }
+double getLatitude(UBXNavPVT &d) { return d.lat / 1e7; } // It divides the latitude received from buffer by 10^7
 double getLongitude(UBXNavPVT &d) { return d.lon / 1e7; }
